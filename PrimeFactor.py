2afdcc602f792e9f31ca70a6dfefa6b1effb8775
TheMaverick888 <themaverick150@gmail.com>
2023-06-05 09:23:33 +0200

    Update PrimeFactor.py

--- PrimeFactor.py
+++ PrimeFactor.py
@@ -2,7 +2,7 @@ def primefactor(x):
     i=2
     while i * i< x:
        while x % i==0:
-             x = x/i  
+             x //= i  
        i+=1         
     print(int(x))         
 primefactor(600851475143)         
